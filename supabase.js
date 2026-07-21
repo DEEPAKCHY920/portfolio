844aab9a72db4a690e4b2ad5ad3225ce31338bbd
DEEPAK CHOUDHARY <Deepak_Choudhary_bca25@jagannathuniversityncr.ac.in>
2026-07-21 19:40:00 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://ygyikpmzxglbfkhqyrgk.supabase.co";
-const SUPABASE_KEY = "sb_publishable_GCE8RKLN9KYEHBn9R9JGkA_x0KE1Oo1";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-
-console.log("✅ supabase.js executed, client keys:", Object.keys(supabase));
